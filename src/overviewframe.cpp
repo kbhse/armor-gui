b0900645c12749c9b1d4d16ab88cd40c820d6aee
username1565 <39200328+username1565@users.noreply.github.com>
2021-10-10 18:49:05 +0000

    Update block-explorer links for AMX mainnet
    Update block-explorer links for AMX mainnet. Change this to http://radioactive.es/AMX-explorer/

--- src/overviewframe.cpp
+++ src/overviewframe.cpp
@@ -22,9 +22,9 @@ namespace WalletGUI {
 
 namespace {
 
-const char MAIN_TX_HASH_URL[] = "https://explorer.armornetwork.org/tx?hash=%1";
-const char MAIN_BLOCK_HASH_URL[] = "https://explorer.armornetwork.org/block?hash=%1";
-const char MAIN_BLOCK_HEIGHT_URL[] = "https://explorer.armornetwork.org/block?height=%1";
+const char MAIN_TX_HASH_URL[] = "http://radioactive.es/AMX-explorer/index.php?raw_tx=%1";
+const char MAIN_BLOCK_HASH_URL[] = "http://radioactive.es/AMX-explorer/index.php?raw_block=%1";
+const char MAIN_BLOCK_HEIGHT_URL[] = "http://radioactive.es/AMX-explorer/index.php?raw_height=%1";
 const char STAGE_TX_HASH_URL[] = "https://stage.explorer.armornetwork.org/tx?hash=%1";
 const char STAGE_BLOCK_HASH_URL[] = "https://stage.explorer.armornetwork.org/block?hash=%1";
 const char STAGE_BLOCK_HEIGHT_URL[] = "https://stage.explorer.armornetwork.org/block?height=%1";
